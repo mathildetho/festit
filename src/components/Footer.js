6af10b43fc3d7a69e40c7f7a4d6bda6da2a49778
GRAND <bengrand.pro@gmail.com>
2020-04-15 23:21:48 +0200

    modif footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,38 +2,58 @@ import React from 'react';
 import "./Footer.css";
 
 
-function Footer() {
-    return(
 
-    <div className = 'Footer'>
+class Footer extends React.Component {
+    constructor(props ){
+        super(props)
+        this.state= {
+            imgfacebook: require('./img/facebook.png')
+        }
+        this.handleMouseOver.bind(this);
+        this.handleMouseOut.bind(this);
+    }
+    handleMouseOver() {
+        this.setState({imgfacebook:require('./img/facebookhover.png')})
+    }
+    handleMouseOut() {
+        this.setState({imgfacebook: require('./img/facebook.png')})
+    }
+    render() {
+        return(
+
+            <div className = 'Footer'>
+                
+                <div className = "footer-content1">
+                    <img className='footer-logo' src = './img/logoFestIt.png' alt='footer logo'/>
+                </div>
+                <div className = "footer-content2">
+                    <p>A propos</p>
+                    <p>Mentions légales</p>
+                    <p>Politique de confidentialité</p>
+                    <p>@The Jackson Three Company | WCS | 2020</p>
+                </div>
+                <div className= "footer-content3">
+                    <p>Rejoindre le club</p>
+                    <input 
+                        className= "footer-input" 
+                        placeholder= "adresse e-mail">
+                    </input>
+                    <button className= "club-button">
+                        <p className= 'send'><strong>Envoyer</strong></p>
+                    </button>
+                    <p>Suivez-nous aussi sur</p>
+                    <img onMouseOver={this.handleMouseOver} onMouseOut={this.handleMouseOut} src={this.state.imgfacebook} alt="facebook-logo" class="logo-style"/>     
+                    <img src="./img/twitter.png" alt="twitter-logo" class="logo-style"/>            
+                    <img src="./img/instagram.png" alt="insta-logo" class="logo-style"/>
         
-        <div className = "footer-content1">
-            <img className='footer-logo' src = './img/logoFestIt.png' alt='footer logo'/>
-        </div>
-        <div className = "footer-content2">
-            <p>A propos</p>
-            <p>Mentions légales</p>
-            <p>Politique de confidentialité</p>
-            <p>@The Jackson Three Company | WCS | 2020</p>
-        </div>
-        <div className= "footer-content3">
-            <p>Rejoindre le club</p>
-            <input 
-                className= "footer-input" 
-                placeholder= "adresse e-mail">
-            </input>
-            <button className= "club-button">
-                <p className= 'send'><strong>Envoyer</strong></p>
-            </button>
-            <p>Suivez-nous aussi sur</p>
-            <img src="./img/facebook.png" alt="facebook-logo" class="logo-style"/>     
-            <img src="./img/twitter.png" alt="twitter-logo" class="logo-style"/>            
-            <img src="./img/instagram.png" alt="insta-logo" class="logo-style"/>
+                    
+                </div>
+            </div>
+            )
+    }
+}
 
-            
-        </div>
-    </div>
-    )
-};
+
+
 
 export default Footer;
