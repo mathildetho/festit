a1994b4ca2ffc0fab4fa93429f0276f23d3c65c1
mathildetho <mathilde.thoraninth@gmail.com>
2020-04-15 11:01:25 +0200

    modify files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      < Footer />
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../../public/img/logoFestIt.png';
+
+const Header = () => {
+    return (
+        <div>
+            <img src={logo} alt='Fest_It' />
+            <nav>
+                
+            </nav>
+        </div>
+    )
+}
+
+export default Header;
