06459ffc3cd6b3c82743d96c885cc238abc7e04c
mathildetho <mathilde.thoraninth@gmail.com>
2020-04-16 12:41:43 +0200

    comment router for the final

--- src/components/Header/Menu.js
+++ src/components/Header/Menu.js
@@ -1,14 +1,15 @@
-import React, {useState} from 'react';
-import { BrowserRouter, Route, NavLink, Link } from "react-router-dom";
+import React, { useState } from "react";
 
 import styled from "styled-components";
-import './Menu.css';
-import logo from '../../img/logoFestIt.png';
+import "./Menu.css";
+import logo from "../../img/logoFestIt.png";
 
-import Festivals from '../Festivals';
+/*import Festivals from '../Festivals';
 import Artistes from '../Artistes';
 import APropos from '../APropos';
-import Panier from '../Panier';
+import Panier from '../Panier'; 
+import { BrowserRouter, Route, NavLink, Link } from "react-router-dom";
+*/
 
 const StyledMenu = styled.nav`
   @media (max-width: 700px) {
@@ -17,30 +18,64 @@ const StyledMenu = styled.nav`
   }
 `;
 
-function Menu ({ open }) {
-    const [imgpanier, setName] = useState(require('../../img/panier.png'));
-    const handleMouseOver = () => setName(require('../../img/panier-rempli.png'));
-    const handleMouseOut = () => setName(require('../../img/panier.png'));
+function Menu({ open }) {
+  const [imgpanier, setName] = useState(require("../../img/panier.png"));
+  const handleMouseOver = () => setName(require("../../img/panier-rempli.png"));
+  const handleMouseOut = () => setName(require("../../img/panier.png"));
 
-        return (
-            <StyledMenu open={open} className="Menu">
-                <BrowserRouter>
-                    <nav className='Nav'>
-                            <Link to="/"><img className='logo' src={logo} alt='logo Fest It' /></Link>
-                            <ul className='menu'>
-                                <li><NavLink activeClassName="active-link" id="title" to="/">Festivals</NavLink></li>
-                                <li><NavLink activeClassName="active-link" id="title" to="/Artistes">Artistes</NavLink></li>
-                                <li><NavLink activeClassName="active-link" id="title" to="/A-propos">À propos</NavLink></li>
-                                <li><Link to="/Panier"><img onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className='panier' src={imgpanier} alt='panier' /></Link></li>
-                            </ul>
-                            <Route path="/" exact component={Festivals} />
-                            <Route path="/Artistes" exact component={Artistes} />
-                            <Route path="/A-propos" exact component={APropos} />
-                            <Route path="/Panier" exact component={Panier} />
-                    </nav>
-                </BrowserRouter>
-            </StyledMenu>
-        )
+  return (
+    <StyledMenu open={open} className="Menu">
+      <nav className="Nav">
+        <a href="#">
+          <img className="logo" src={logo} alt="logo Fest It" />
+        </a>
+        <ul className="menu">
+          <li>
+            <a id="title" href="#">
+              Festivals
+            </a>
+          </li>
+          <li>
+            <a id="title" href="#">
+              Artistes
+            </a>
+          </li>
+          <li>
+            <a id="title" href="#">
+              À propos
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img
+                onMouseOver={handleMouseOver}
+                onMouseOut={handleMouseOut}
+                className="panier"
+                src={imgpanier}
+                alt="panier"
+              />
+            </a>
+          </li>
+        </ul>
+      </nav>
+      {/*
+        <BrowserRouter>
+            <nav className='Nav'>
+                <Link to="/"><img className='logo' src={logo} alt='logo Fest It' /></Link>
+                <ul className='menu'>
+                    <li><NavLink activeClassName="active-link" id="title" to="/">Festivals</NavLink></li>
+                    <li><NavLink activeClassName="active-link" id="title" to="/Artistes">Artistes</NavLink></li>
+                    <li><NavLink activeClassName="active-link" id="title" to="/A-propos">À propos</NavLink></li>
+                    <li><Link to="/Panier"><img onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className='panier' src={imgpanier} alt='panier' /></Link></li>
+                </ul>
+            <Route path="/" exact component={Festivals} />
+            <Route path="/Artistes" exact component={Artistes} />
+            <Route path="/A-propos" exact component={APropos} />
+            <Route path="/Panier" exact component={Panier} />
+            </nav>
+        </BrowserRouter>*/}
+    </StyledMenu>
+  );
 }
 
 export default Menu;
