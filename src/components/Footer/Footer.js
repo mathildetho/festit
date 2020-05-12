9e78025822b8683ac5228b58b05ce72cf95d0f99
mathildetho <mathilde.thoraninth@gmail.com>
2020-05-12 19:04:16 +0200

    clean footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -57,40 +57,49 @@ class Footer extends React.Component {
                     <Link to="/A-propos" className='footer-link'>À propos</Link>
                     <a className='footer-link' href='null'>Mentions légales</a>
                     <a className='footer-link' href='null'>Politique de confidentialité</a>
-                    <a className='footer-link' href='null'>@The Jackson Three Company | WCS | 2020</a>     
+                    <p className='footer-link'>@The Jackson Three Company | <a href='https://www.wildcodeschool.com/fr-FR'>WCS</a> | 2020</p>     
                 </div>
                 
                 <div className= "footer-content3">
                     <p>Rejoindre le club</p>
-                    <input 
+                    <form><input 
                         className= "footer-input" 
                         placeholder= "adresse e-mail">
                     </input>
                     <button className= "club-button">
                         <p className= 'send'><strong>Envoyer</strong></p>
                     </button>
+                    </form>
                     <p>Suivez-nous aussi sur</p>
-                    <img 
-                    onMouseOver={() => this.handleMouseOverFb()} 
-                    onMouseOut={() => this.handleMouseOutFb()} 
-                    src={this.state.imgfacebook} 
-                    alt="facebook-logo" 
-                    className="social-logo"
-                    />  
-                    <img
-                        onMouseOver={() => this.handleMouseOverTw()}
-                        onMouseOut={() => this.handleMouseOutTw()} 
-                        src={this.state.imgtwitter}
-                        alt="twitter-logo" 
-                        className="social-logo"
-                        />
-                    <img 
-                        onMouseOver={() => this.handleMouseOverInsta()}
-                        onMouseOut={() => this.handleMouseOutInsta()} 
-                        src={this.state.imginstagram}
-                        alt="insta-logo" 
-                        className="social-logo"
-                        />
+                    <div className="rs">
+                        <a href='https://www.facebook.com/'>
+                            <img 
+                            onMouseOver={() => this.handleMouseOverFb()} 
+                            onMouseOut={() => this.handleMouseOutFb()} 
+                            src={this.state.imgfacebook} 
+                            alt="facebook-logo" 
+                            className="social-logo"
+                            />  
+                        </a>
+                        <a href='https://twitter.com/'>
+                            <img
+                            onMouseOver={() => this.handleMouseOverTw()}
+                            onMouseOut={() => this.handleMouseOutTw()} 
+                            src={this.state.imgtwitter}
+                            alt="twitter-logo" 
+                            className="social-logo"
+                            />
+                        </a>
+                        <a href='https://www.instagram.com/?hl=fr'>
+                            <img 
+                            onMouseOver={() => this.handleMouseOverInsta()}
+                            onMouseOut={() => this.handleMouseOutInsta()} 
+                            src={this.state.imginstagram}
+                            alt="insta-logo" 
+                            className="social-logo"
+                            />
+                        </a>
+                    </div>
                 </div>
             </div>
             )
