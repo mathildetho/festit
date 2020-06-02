e320dea7416631bdcd57cda226b13a3e7aee9de8
mathildetho <mathilde.thoraninth@gmail.com>
2020-06-02 08:56:49 +0200

    change img line-up and text Mathilde

--- src/components/APropos/AboutLineUp.js
+++ src/components/APropos/AboutLineUp.js
@@ -2,12 +2,12 @@ import React from 'react';
 import AboutCard from './AboutCard';
 
 import ben from '../../img/benoit.jpg';
-import thom from '../../img/thomas.jpg';
+import thom from '../../img/thomas.png';
 import math from '../../img/mathilde.jpg';
 
 const AboutLineUp = () => {
     const desc1 = 'Joueur de triangle pour l’orchestre  National de Roumanie de 1997 à 2002.';
-    const desc2 = 'Passionnée de TrashMetal et de Hardcore depuis sa plus tendre enfance, elle s’est ensuite tourné vers le dev';
+    const desc2 = 'Passionnée par le design et le web, c\'est tout naturellement qu\'elle se tourne vers le développement web.';
     const desc3 = 'Après avoir assisté à un concert de Chantal Goya à l\'âge de 28 ans, Thomas décide de consacrer sa vie à la musique et au développement web.'
 
     const link1 = 'https://www.linkedin.com/in/benoitgrand/';
